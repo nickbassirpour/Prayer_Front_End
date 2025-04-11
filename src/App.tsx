d701625708ae962bc2475a575bf7f898c7bcd151
nickbassirpour <nickbassirpour@gmail.com>
2025-04-10 20:40:58 -0700

    tailwind and initial image

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import stIgnatius from "./assets/st_ignatius.png";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <section>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://traditioninaction.org">
+          <img
+            src={stIgnatius}
+            alt="Saint of the Day"
+            className="w-full h-64 object-cover"
+          />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <div>
+        <h1 className="bg-blue-500">St Ignatius</h1>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur,
+          odit. Id vel dolorum debitis deserunt cupiditate numquam excepturi
+          voluptas eligendi aperiam quidem nisi nostrum maiores nobis maxime
+          eum, distinctio qui?
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </section>
+  );
 }
 
-export default App
+export default App;
