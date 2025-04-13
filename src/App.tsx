2271d951b384cb3f37935d8daa5f7af114196d45
nickbassirpour <nickbassirpour@gmail.com>
2025-04-12 20:32:35 -0700

    ADding hamburger menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,30 @@
-import { useState } from "react";
+import Navbar from "./components/Navbar";
 import stIgnatius from "./assets/st_ignatius.png";
 import "./App.css";
 
 function App() {
   return (
-    <section className="grid gap-2 md:grid-cols-2 md:items-center md:text-left">
-      <div>
-        <a href="https://traditioninaction.org">
-          <img
-            src={stIgnatius}
-            alt="Saint of the Day"
-            className="w-full h-64 object-cover"
-          />
-        </a>
-      </div>
-      <div>
-        <h1 className="mb-2 text-3xl font-medium">St Ignatius</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur,
-          odit. Id vel dolorum debitis deserunt cupiditate numquam excepturi
-          voluptas eligendi aperiam quidem nisi nostrum maiores nobis maxime
-          eum, distinctio qui?
-        </p>
-      </div>
-    </section>
+    <>
+      <Navbar />
+      <section className="grid gap-2 md:grid-cols-2 md:items-center md:text-left">
+        <div>
+          <a href="https://traditioninaction.org">
+            <img
+              src={stIgnatius}
+              alt="Saint of the Day"
+              className="w-full h-64 object-cover"
+            />
+          </a>
+        </div>
+        <div>
+          <h1 className="mb-2 text-2xl font-medium">St Ignatius - June 12th</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Consequatur, odit...
+          </p>
+        </div>
+      </section>
+    </>
   );
 }
 
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,92 @@
+import { Fragment, useState } from "react";
+import {
+  Dialog,
+  DialogPanel,
+  Transition,
+  TransitionChild,
+} from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+
+const navigation = [
+  { name: "Home", href: "#" },
+  { name: "Saints", href: "#" },
+  { name: "Articles", href: "#" },
+];
+
+export default function Navbar() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  return (
+    <header className="absolute top-0 left-0 w-full z-20 md:static md:bg-white md:border-b">
+      <div className="mx-auto max-w-7x1 px-4 sm:px-6 lg:px-8 flex items-center justify-between h-16">
+        {/* Logo here*/}
+        <div className="text-lg font-bold hidden md:block">Catholic Saints</div>
+
+        {/* Hamburder for mobile */}
+        <div className="md:hidden ml-auto">
+          <button
+            onClick={() => setMobileMenuOpen(true)}
+            className="text-white p-2 bg-black/30-rounded"
+          >
+            <Bars3Icon className="h-8 w-8" />
+          </button>
+        </div>
+
+        {/* Desktop nav */}
+        <nav className="hidden md:flex space-x-6">
+          {navigation.map((item) => (
+            <a
+              key={item.name}
+              href={item.href}
+              className="text-gray-700 hover:text-blue-500"
+            >
+              {item.name}
+            </a>
+          ))}
+        </nav>
+      </div>
+
+      {/* Mobile menu */}
+      <Transition show={mobileMenuOpen} as={Fragment}>
+        <Dialog
+          as="div"
+          className="md:hidden relative z-10"
+          open={mobileMenuOpen}
+          onClose={() => setMobileMenuOpen(false)}
+        >
+          <TransitionChild
+            as={Fragment}
+            enter="transition ease-out duration-1000"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="transition ease-in duration-1000"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black/50" />
+          </TransitionChild>
+
+          <DialogPanel>
+            <div className="fixed inset-y-0 left-0 w-64 bg-white shadow-md p-4">
+              <button onClick={() => setMobileMenuOpen(false)} className="mb-4">
+                <XMarkIcon className="h-6 w-6 text-gray-700" />
+              </button>
+
+              <nav className="space-y-4">
+                {navigation.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    className="block text-gray-800"
+                  >
+                    {item.name}
+                  </a>
+                ))}
+              </nav>
+            </div>
+          </DialogPanel>
+        </Dialog>
+      </Transition>
+    </header>
+  );
+}
